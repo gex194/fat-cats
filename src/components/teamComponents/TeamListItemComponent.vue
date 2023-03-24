2f69892c000a4b7fe2bb061e732e5e71e15b437d
Andrei Vasilev <andrei.vasilev@crplab.ru>
2023-03-24 19:27:23 +0300

    [ADD] added new members of our team, replaced pngs with webps

--- src/components/teamComponents/TeamListItemComponent.vue
+++ src/components/teamComponents/TeamListItemComponent.vue
@@ -11,14 +11,14 @@
       <div class="v-col-lg-6 v-col-sm-12 d-flex align-center">
         <v-sheet
           class="d-flex bg-transparent align-center justify-center"
-          :class="is_even_or_zero(index) ? 'justify-lg-start align-lg-end' : 'justify-lg-end align-lg-start'"
-          width="100%">
-          <v-img
-            eager
-            class="rounded-xl"
-            style="max-width: 50vh;"
-            :src="img"
-          />
+          :class="
+            is_even_or_zero(index)
+              ? 'justify-lg-start align-lg-end'
+              : 'justify-lg-end align-lg-start'
+          "
+          width="100%"
+        >
+          <v-img eager class="rounded-xl" style="max-width: 50vh" :src="img" />
         </v-sheet>
       </div>
       <div
@@ -33,11 +33,11 @@
           </v-card-title>
         </v-card-item>
         <v-card-item class="d-flex">
-          <v-card-subtitle
-            >
-            <span class="text-subtitle-1 text-lg-h5 text-md-h5 text-sm-h5">{{ role }}</span>
-          </v-card-subtitle
-          >
+          <v-card-subtitle>
+            <span class="text-subtitle-1 text-lg-h5 text-md-h5 text-sm-h5">{{
+              role
+            }}</span>
+          </v-card-subtitle>
         </v-card-item>
         <div class="d-flex">
           <v-card-item
@@ -46,12 +46,14 @@
             class="d-flex justify-center flex-row"
           >
             <v-btn
-              :icon="link.icon"
+              icon
               target="_blank"
               :href="link.url"
               color="amber-accent-1"
               class="text-black"
-            />
+            >
+              <i class="btn-icon" :class="link.icon"></i>
+            </v-btn>
           </v-card-item>
         </div>
       </div>
@@ -83,5 +85,9 @@ export default {
 }
 .visible {
   visibility: visible;
+}
+
+.btn-icon {
+  font-size: 1.5rem;
 }
 </style>
